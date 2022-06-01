83dd694c1c36ff087639b36f084d274c4e05282d
Sandra Tena <sandratena06@gmail.com>
2022-06-01 14:42:08 -0700

    Add functions to scripts.js
    Co-authored-by: Peter Vang <vang_peter06@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,30 @@
 $(document).ready(function() {
-  
+  $(".clickable").click(function() {
+    $("#javascript-word").toggle();
+    $("#javascript-definition").toggle();
+  })
+  $(".clickable").click(function() {
+    $("#operators-word").toggle();
+    $("#operartors-definition").toggle();
+  })
+  $(".clickable").click(function() {
+    $("#variables-word").toggle();
+    $("#variables-definition").toggle();
+  })
+  $(".clickable").click(function() {
+    $("#naming-word").toggle();
+    $("#naming-definition").toggle();
+  })
+  $(".clickable").click(function() {
+    $("#functions-word").toggle();
+    $("#functions-definition").toggle();
+  })
+  $(".clickable").click(function() {
+    $("#methods-word").toggle();
+    $("#methods-definition").toggle();
+  })
+  $(".clickable").click(function() {
+    $("#arguments-word").toggle();
+    $("#arguments-definition").toggle();
+  })
 });
